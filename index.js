d92cbbfb40e23760213805f15629f5dc6faad17b
Kelvan Ince <incekelvan@gmail.com>
2018-03-28 20:01:25 -0500

    Add cluster fork method from the cluster module

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-app.get('/', (req, res) => {
-  res.send('Hi');
-});
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // index.js is to be executed again but in child mode.
+  cluster.fork();
+} else {
+// I'm a child. I'll act like a server and do nothing else.
+  const express = require('express');
+  const app = express();
 
-app.listen(3000);
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi');
+  });
+
+  app.listen(3000);
+}
