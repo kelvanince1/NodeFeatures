bbac5a312bfface5d8851124dd63dd34221ac21a
Kelvan Ince <incekelvan@gmail.com>
2018-03-29 20:05:22 -0500

    Remove clustering

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-const cluster = require('cluster');
+const express = require('express');
+const app = express();
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // index.js is to be executed again but in child mode.
-  cluster.fork();
-} else {
-// I'm a child. I'll act like a server and do nothing else.
-  const express = require('express');
-  const app = express();
-
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
-  app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi');
-  });
-
-  app.get('/fast', (req, res) => {
-    res.send('Fast');
-  })
-
-  app.listen(3000);
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
 }
+
+app.get('/', (req, res) => {
+  doWork(5000);
+  res.send('Hi');
+});
+
+app.get('/fast', (req, res) => {
+  res.send('Fast');
+})
+
+app.listen(3000);
