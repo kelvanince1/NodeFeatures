131cf1e78f413535941095a3c909d9f1b4f17717
Kelvan Ince <incekelvan@gmail.com>
2018-04-01 14:20:08 -0500

    Automate the cache clearing by adding an asynchronous middleware to the POSTing of a new blog

--- AdvancedNodeStarter/routes/blogRoutes.js
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const { clearHash } = require('../services/cache');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -20,7 +20,7 @@ module.exports = app => {
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -35,6 +35,5 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
-    clearHash(req.user.id);
   });
 };
