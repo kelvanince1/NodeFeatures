ecace74e8ea04e749bae8753f96608441b746f9e
Kelvan Ince <incekelvan@gmail.com>
2018-04-01 13:23:03 -0500

    Modify blog routes to handle all Redis query cases. Async Await the result of the query to Mongo or Redis in cache file

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log('About to run a query');
-  
-  return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+
+    // See if we have a value for 'key' in Redis
+    const cacheValue = await client.get(key)
+
+    // If so, return it
+    if (cacheValue) {
+      console.log(cacheValue);
+    }
+
+    // Otherwise, issue the query and store result in Redis
+    await result = await exec.apply(this, arguments);
+    console.log(result);
 }
