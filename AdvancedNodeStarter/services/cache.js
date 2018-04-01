df1c6468c5615644b81d760fcc6a6aeb52c6fbc8
Kelvan Ince <incekelvan@gmail.com>
2018-04-01 12:49:58 -0500

    Add cache file which will handle a query to Redis prior to a call to Mongo being made

---
+++ AdvancedNodeStarter/services/cache.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function() {
+  console.log('About to run a query');
+  
+  return exec.apply(this, arguments);
+}
