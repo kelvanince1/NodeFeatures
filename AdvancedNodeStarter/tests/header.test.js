d554d1458b3c68a0ffdb67886b397a972f874951
Kelvan Ince <incekelvan@gmail.com>
2018-04-05 20:35:14 -0500

    Add Passport.js waitFor function to allow enough time for Chromium to read the tests

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -50,4 +50,9 @@ test.only('When signed in, show logout button', async () => {
   await page.setCookie({ name: 'session.sig', value: sig });
 
   await page.goto('localhost:3000');
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
 });
