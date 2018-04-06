b045d7158b2a66491db0c435518b789308e86964
Kelvan Ince <incekelvan@gmail.com>
2018-04-05 20:13:02 -0500

    Add test to see if the logout button is displayed upon a successful authentication

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
+const keys = require('../config/keys');
 
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
-
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function(options = {}) {
@@ -13,36 +12,38 @@ mongoose.Query.prototype.cache = function(options = {}) {
   this.hashKey = JSON.stringify(options.key || '');
 
   return this;
-}
+};
 
 mongoose.Query.prototype.exec = async function() {
   if (!this.useCache) {
     return exec.apply(this, arguments);
   }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
-    }));
+    })
+  );
 
-    // See if we have a value for 'key' in Redis
-    const cacheValue = await client.hget(this.hashKey, key);
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.hget(this.hashKey, key);
 
-    // If so, return it
-    if (cacheValue) {
-      const doc = JSON.parse(cacheValue);
+  // If we do, return that
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
 
-      return Array.isArray(doc)
+    return Array.isArray(doc)
       ? doc.map(d => new this.model(d))
       : new this.model(doc);
-    }
+  }
 
-    // Otherwise, issue the query and store result in Redis
-    const result = await exec.apply(this, arguments);
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
 
-    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
-    return result;
-}
+  return result;
+};
 
 module.exports = {
   clearHash(hashKey) {
--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -27,3 +27,25 @@ test('Clicking login starts oAuth flow', async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test('When signed in, show logout button', async () => {
+  const id = '5abec76870c93f0a2aaf51c7';
+
+  const Buffer = require('safe-buffer').Buffer;
+
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  console.log('String', sessionString);
+  console.log('Sig', sig);
+});
