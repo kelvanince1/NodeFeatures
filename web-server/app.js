39f770ca4ef0516c25df884e7c578939cc395b09
Kelvan Ince <incekelvan@gmail.com>
2018-07-02 19:23:12 -0500

    Added HTML file to be rendered

--- web-server/app.js
+++ web-server/app.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const fs = require('fs');
+
+
 
 http.createServer(function(req, res) {
   res.writeHead(200, {
-    'Content': 'text/plain'
+    'Content': 'text/html'
   });
-  res.end('Hello World\n');
+  const html = fs.readFileSync(__dirname + '/index.html');
+  res.end(html);
 }).listen(1337, '127.0.0.1');
