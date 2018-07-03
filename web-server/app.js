3b393eca75d3227bc1a9670090e4614573116dd7
Kelvan Ince <incekelvan@gmail.com>
2018-07-02 21:52:00 -0500

    Add some basic routing to app

--- web-server/app.js
+++ web-server/app.js
@@ -1,17 +1,36 @@
 const http = require('http');
 const fs = require('fs');
 
-// Send data using pipe
+// Handle paths
 http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content': 'application/json'
-  });
-  const obj = {
-    firstName: 'John',
-    lastName: 'Doe'
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  }
+  if (req.url === '/api') {
+    res.writeHead(200, {
+      'Content': 'application/json'
+    });
+    const obj = {
+      firstName: 'John',
+      lastName: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
   };
-  res.end(JSON.stringify(obj));
+  res.writeHead(404);
+  res.end();
 }).listen(1337, '127.0.0.1');
+
+// Send json data
+// http.createServer(function(req, res) {
+//   res.writeHead(200, {
+//     'Content': 'application/json'
+//   });
+//   const obj = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+//   };
+//   res.end(JSON.stringify(obj));
+// }).listen(1337, '127.0.0.1');
 
 
 // Send data using pipe
