b50ea2627a84ef025f9e9bd01f4be8d04c62d202
Kelvan Ince <incekelvan@gmail.com>
2018-07-02 20:16:12 -0500

    Send data response through a stream and pipe

--- web-server/app.js
+++ web-server/app.js
@@ -1,12 +1,22 @@
 const http = require('http');
 const fs = require('fs');
 
-
-
+// Send data using pipe
 http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content': 'text/html'
   });
-  const html = fs.readFileSync(__dirname + '/index.html');
-  res.end(html);
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
 }).listen(1337, '127.0.0.1');
+
+
+// Read data as html file
+// http.createServer(function(req, res) {
+//   res.writeHead(200, {
+//     'Content': 'text/html'
+//   });
+//   let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+//   let message = 'Hello World...';
+//   html = html.replace('{Message}', message);
+//   res.end(html);
+// }).listen(1337, '127.0.0.1');
