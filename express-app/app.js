f1df8ee664fa28fb70c20f3c10400288f0e54ff1
Kelvan Ince <incekelvan@gmail.com>
2018-07-04 12:06:06 -0500

    Add some remaining RESTful method calls

--- express-app/app.js
+++ express-app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
 
 const app = express();
 
@@ -19,13 +20,35 @@ app.get('/api', function(req, res) {
   res.json({ firstName: 'John', lastName: 'Doe' })
 });
 
+app.get('/api/:id', function(req, res) {
+  // Get data from database
+  res.render('person', { ID: req.params.id, Qstr: req.query.value });
+});
+
 app.post('/api', urlencodedParser, function(req, res) {
   res.send('Thanks');
   console.log(req.body.firstName);
   console.log(req.body.lastName);
 });
 
-app.get('/api/:id', function(req, res) {
+app.post('/api', jsonParser, function(req, res) {
+  res.send('Thanks for the json data');
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
+});
+
+app.get('/api/person/:id', function(req, res) {
+  // Get data from database
+  res.json({ firstName: 'John', lastName: 'Doe' })
+});
+
+app.post('/api/person', function(req, res) {
+  // Get data from database
+  res.render('person', { ID: req.params.id, Qstr: req.query.value });
+});
+
+app.delete('/api/person/:id', function(req, res) {
+  // Get data from database
   res.render('person', { ID: req.params.id, Qstr: req.query.value });
 });
 
