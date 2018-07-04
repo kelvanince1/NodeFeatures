f5a975800ca91a6801a46ec4396b360b7762fde0
Kelvan Ince <incekelvan@gmail.com>
2018-07-03 20:47:04 -0500

    Add express setup app

---
+++ express-app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.send('<html><head></head><body><h1>Hello World</h1></body></html>')
+});
+
+app.get('/api', function(req, res) {
+  res.json({ firstName: 'John', lastName: 'Doe' })
+});
+
+app.get('/api/:id', function(req, res) {
+  res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>')
+});
+
+app.listen(port);
