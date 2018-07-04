3390a7751a7539446e431295eb6e1c8349ea1fda
Kelvan Ince <incekelvan@gmail.com>
2018-07-03 23:43:53 -0500

    Add styling to home route

--- express-app/app.js
+++ express-app/app.js
@@ -4,8 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-  res.send('<html><head></head><body><h1>Hello World</h1></body></html>')
+  res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World</h1></body></html>')
 });
 
 app.get('/api', function(req, res) {
