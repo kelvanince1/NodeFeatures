1d8d34d345885621a24ec02c9c4313a47de89247
Kelvan Ince <incekelvan@gmail.com>
2018-07-04 20:07:42 -0500

    Functionality to store data in Mongo

--- db-express/app.js
+++ db-express/app.js
@@ -7,6 +7,8 @@ const htmlController = require('./controllers/htmlController');
 
 const port = process.env.PORT || 3000;
 
+import { DB_USER, DB_PASSWORD } from './config';
+
 mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@ds125871.mlab.com:25871/js-course-db`);
 
 const Schema = mongoose.Schema;
@@ -17,7 +19,19 @@ const personSchema = new Schema({
 	address: String
 });
 
-const person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema);
+
+const john = Person({
+	firstName: 'John',
+	lastName: 'Doe',
+	address: '555 Main Ave'
+});
+
+john.save(function(err) {
+	if (err) throw err;
+
+	console.log('Person saved');
+});
 
 app.use('/assets', express.static(__dirname + '/public'));
 
