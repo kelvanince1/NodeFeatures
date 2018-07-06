5f70c2e336f9c8532862a86e3756cf3ad1849ebf
Kelvan Ince <incekelvan@gmail.com>
2018-07-05 20:18:36 -0500

    Add mongoose connection

--- node-todo/app.js
+++ node-todo/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const config = require('./config');
 
 let port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionString)
 
 app.listen(port);
