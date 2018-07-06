418505780b27908570debb870fa2b8988b9df44f
kelvanince1 <incekelvan@gmail.com>
2018-07-06 10:01:20 -0500

    Add API and the methods to add and retrieve todos

--- node-todo/app.js
+++ node-todo/app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var config = require('./config');
-var setupController = require('./controllers/setupController');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config');
+const setupController = require('./controllers/setupController');
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
