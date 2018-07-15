b004c3dc354ebcdef8061b41e988af30c2465f0c
Kelvan Ince <incekelvan@gmail.com>
2018-07-15 13:54:36 -0500

    Slight modification to test web server

--- web-server/app.js
+++ web-server/app.js
@@ -19,36 +19,3 @@ http.createServer(function(req, res) {
   res.writeHead(404);
   res.end();
 }).listen(1337, '127.0.0.1');
-
-// Send json data
-// http.createServer(function(req, res) {
-//   res.writeHead(200, {
-//     'Content': 'application/json'
-//   });
-//   const obj = {
-//     firstName: 'John',
-//     lastName: 'Doe'
-//   };
-//   res.end(JSON.stringify(obj));
-// }).listen(1337, '127.0.0.1');
-
-
-// Send data using pipe
-// http.createServer(function(req, res) {
-//   res.writeHead(200, {
-//     'Content': 'text/html'
-//   });
-//   fs.createReadStream(__dirname + '/index.html').pipe(res);
-// }).listen(1337, '127.0.0.1');
-
-
-// Read data as html file
-// http.createServer(function(req, res) {
-//   res.writeHead(200, {
-//     'Content': 'text/html'
-//   });
-//   let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-//   let message = 'Hello World...';
-//   html = html.replace('{Message}', message);
-//   res.end(html);
-// }).listen(1337, '127.0.0.1');
