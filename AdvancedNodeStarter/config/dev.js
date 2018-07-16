9493207a3c86c027c279b77e4e7c2cb2af44a790
kelvanince1 <incekelvan@gmail.com>
2018-07-16 09:44:35 -0500

    Add two started apps

---
+++ AdvancedNodeStarter/config/dev.js
@@ -0,0 +1,9 @@
+// Don't be like me and commit this file!
+// These keys have been disabled, but remain here for reference purposes.
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://kelvanince:darthvader1@ds229609.mlab.com:29609/blog_dev',
+  cookieKey: '123123123'
+};
