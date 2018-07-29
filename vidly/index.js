b1597ca59c4e9e387217437acb6058a924033739
Kelvan Ince <incekelvan@gmail.com>
2018-07-29 17:12:59 -0500

    Add Mongo responses to each route

--- vidly/index.js
+++ vidly/index.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const Joi = require('joi');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('connected to mongo'))
-  .catch(err => console.error('Could not connect', err))
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
