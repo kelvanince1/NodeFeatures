d9b32d404bbfcd951bc2cb3c4ac5f281467fdcc7
Kelvan Ince <incekelvan@gmail.com>
2018-07-28 20:38:10 -0500

    Add Mongoose Schema and querying function

--- mongo-app/index.js
+++ mongo-app/index.js
@@ -8,3 +8,41 @@ mongoose.connect('mongodb://localhost/playground')
   .catch((err) => {
     console.error('Could not connnect to MongoDB', err)
   })
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+  const Course = mongoose.model('Course', courseSchema);
+  const course = new Course({
+    name: 'Node.js Course',
+    author: 'Kelvan',
+    tags: ['Node', 'Backend'],
+    isPublished: true
+  });
+  const result = await course.save();
+  console.log(result);
+}
+
+// Comparison operators available in MongoDB
+// eq - equal
+// ne - not equal
+// gt - greater than
+// gte - greater than or equal to
+// lt - less than
+// lte - less than or equal to
+// in
+// nin - not in
+
+async function getCourses() {
+  const res = await Course.find();
+  console.log('RES', res);
+}
+
+getCourses();
