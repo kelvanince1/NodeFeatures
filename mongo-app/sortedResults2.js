2241527ecac1210b6671b74dc95762659c7a694f
Kelvan Ince <incekelvan@gmail.com>
2018-07-28 21:55:54 -0500

    Add solutions using differing filtering methods

---
+++ mongo-app/sortedResults2.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// Obviously this changed to a url in production
+mongoose.connect('mongodb://localhost/mongo-exercises')
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((err) => {
+    console.error('Could not connnect to MongoDB', err)
+  })
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+  return course = await Course
+    .find({ isPublished: true, tags: { $in: ['frontend', 'backend'] } })
+    .sort({ price: -1 })
+    .select({ name: 1, author: 1, price: 1 })
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+};
+
+run();
