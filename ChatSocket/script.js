3c558228a0b611a032341ec16e286a5603be52f6
Kelvan Ince <incekelvan@gmail.com>
2020-03-23 18:25:00 -0400

    Add broadcast to show new user in application

--- ChatSocket/script.js
+++ ChatSocket/script.js
@@ -3,8 +3,16 @@ const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 const messageContainer = document.getElementById('message-container');
 
+const name = prompt('What is your name?');
+appendMessage('You joined');
+socket.emit('new-user', name);
+
 socket.on('chat-message', data => {
     appendMessage(data);
+});
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`);
 });
 
 messageForm.addEventListener('submit', e => {
--- ChatSocket/server.js
+++ ChatSocket/server.js
@@ -1,8 +1,14 @@
 const io = require('socket.io')(3000);
 
+const users = {};
+
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World');
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    });
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', message);
     });
 });
+
