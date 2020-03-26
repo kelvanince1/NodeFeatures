7496978314d261b61d881d3781a6c5a24472ea9a
Kelvan Ince <incekelvan@gmail.com>
2020-03-25 21:10:54 -0400

    Only allow sockets to be broadcast to appropariate room

--- ChatSocket/server.js
+++ ChatSocket/server.js
@@ -53,7 +53,7 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         getUserRooms(socket)
             .forEach(room => {
-                socket.broadcast.emit('user-disconnected', rooms[room].users[socket.id]);
+                socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id]);
                 delete rooms[room].users[socket.id];
             })
     });
